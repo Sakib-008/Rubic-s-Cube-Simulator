685e16c5ea1c52b4246d1b40a024f5013cd6a696
Sakib-008 <kzsakibcr@gmail.com>
2025-12-23 10:18:05 +0600

    Initial Commit

---
+++ code.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<vector<char>> cube = {{'W', 'W', 'W', 'W'}, {'R', 'R', 'R', 'R'}, {'B', 'B', 'B', 'B'}, {'O', 'O', 'O', 'O'}, {'G', 'G', 'G', 'G'}, {'Y', 'Y', 'Y', 'Y'}};
+    int n;
+    cout << "Enter number of moves :";
+    cin >> n;
+    while (n--)
+    {
+        string move;
+        cout << "Enter move :";
+        cin >> move;
+        char m = move[0];
+        char face = move[1];
+        int f;
+        if (face == 'U')
+            f = 0;
+        else if (face == 'D')
+            f = 1;
+        else if (face == 'L')
+            f = 2;
+        else if (face == 'R')
+            f = 3;
+
+        if (m == '1')
+        {
+            if (f == 0)
+            {
+                char temp1 = cube[0][1];
+                char temp2 = cube[0][3];
+                cube[0][1] = cube[5][1];
+                cube[0][3] = cube[5][3];
+                cube[5][1] = cube[4][1];
+                cube[5][3] = cube[4][3];
+                cube[4][1] = cube[3][1];
+                cube[4][3] = cube[3][3];
+                cube[3][1] = temp1;
+                cube[3][3] = temp2;
+            }
+            else if (f == 1)
+            {
+                char t1 = cube[3][1];
+                char t2 = cube[3][3];
+                cube[3][1] = cube[4][1];
+                cube[3][3] = cube[4][3];
+                cube[4][1] = cube[5][1];
+                cube[4][3] = cube[5][3];
+                cube[5][1] = cube[0][1];
+                cube[5][3] = cube[0][3];
+                cube[0][1] = t1;
+                cube[0][3] = t2;
+            }
+            else if (f == 2)
+            {
+                char t1 = cube[0][2];
+                char t2 = cube[0][3];
+                cube[0][2] = cube[1][2];
+                cube[0][3] = cube[1][3];
+                cube[1][2] = cube[4][2];
+                cube[1][3] = cube[4][3];
+                cube[4][2] = cube[2][2];
+                cube[4][3] = cube[2][3];
+                cube[2][2] = t1;
+                cube[2][3] = t2;
+            }
+            else if (f == 3)
+            {
+                char t1 = cube[2][2];
+                char t2 = cube[2][3];
+                cube[2][2] = cube[4][2];
+                cube[2][3] = cube[4][3];
+                cube[4][2] = cube[1][2];
+                cube[4][3] = cube[1][3];
+                cube[1][2] = cube[0][2];
+                cube[1][3] = cube[0][3];
+                cube[0][2] = t1;
+                cube[0][3] = t2;
+            }
+        }
+    }
+}
